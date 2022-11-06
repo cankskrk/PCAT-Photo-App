54b8a9e16d6a027f951caf7b591a85132e78ffd2
cankskrk <ardacankisakurek@gmail.com>
2022-11-06 02:45:38 +0100

    Update Workspace

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+// Template Engine
+app.set('view engine', 'ejs');
+
 // Middlewares
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  console.log('Received Data!');
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
