a0396444089e94321dfcd91dd8e278ffd85956ec
cankskrk <ardacankisakurek@gmail.com>
2022-11-07 21:27:47 +0100

    Update README File

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## PCAT Photo App
+
+<br>
+
+![](./screenshots/PCAT-ScreenShot.png)
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 const Photo = require('./models/Photo');
 
 // Connect DB
