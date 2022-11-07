22c383ff4b683041f86549614c4d0ea2980d0401
cankskrk <ardacankisakurek@gmail.com>
2022-11-07 03:18:08 +0100

    add MongoDB Operation

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
+
 const app = express();
 const port = 3000;
+const Photo = require('./models/Photo');
+
+// Connect DB
+mongoose.connect('mongodb://127.0.0.1/pcat-db');
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos, // photos: photos seklinde yazmaya gerek yok.
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -23,6 +33,12 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
+// Listening PORT
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}...`);
 });
