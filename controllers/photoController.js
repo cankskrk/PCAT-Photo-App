1affe61596dbc9be4625a39f281254bbc1a8cf34
cankskrk <ardacankisakurek@gmail.com>
2022-11-18 11:45:09 +0100

    Add Pagination Operation

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,9 +2,19 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated'); // Oluşturulma tarihine göre sıraladık. "-" nin amacı en son oluşturulanın ilk başa geçmesini sağlamaktır.
+  const page = req.query.page || 1; // query olmazsa 1 olacak
+  const photosPerPage = 3; // Her sayfada  kac tane icerik olacak
+  const totalPhotos = await Photo.find().countDocuments(); // Dokuman sayisi beirleme
+
+  const photos = await Photo.find({}) // Goruntulemek icin tum dokumanlari al
+    .sort('-dateCreated') // Oluşturulma tarihine göre sıraladık. "-" nin amacı en son oluşturulanın ilk başa geçmesini sağlamaktır.
+    .skip((page - 1) * photosPerPage) // 4 tane dokumani gececek
+    .limit(photosPerPage);
+
   res.render('index', {
     photos, // photos: photos seklinde yazmaya gerek yok.
+    current: page, // Anlik sayfa
+    pages: Math.ceil(totalPhotos / photosPerPage), // Tum dokuman sayisi / Sayfa basina dokuman // Asagi yuvarlarsak son sayfada kalan ve sayfayi doldurmayan dokumanlar gozukmez.
   });
 };
 
