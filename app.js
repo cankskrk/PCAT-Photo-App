529185924fbbf0236fb38474714c3c395a457273
cankskrk <ardacankisakurek@gmail.com>
2022-11-19 08:46:58 +0100

    Ready For Heroku

--- app.js
+++ app.js
@@ -11,13 +11,10 @@ const pageController = require('./controllers/pageController');
 
 // Connect DB
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.jbsxgf3.mongodb.net/?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(`mongodb://127.0.0.1:27017`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('DB Connected!');
   })
